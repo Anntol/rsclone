eaecd5d4ad45e441ffce4a78860cfd109c64191a
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-01-10 14:04:15 +0300

    add: angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
