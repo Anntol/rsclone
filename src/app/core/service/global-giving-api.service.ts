07873b6d0411b75cffbcc3114d4f1e5e3a09e960
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-01-10 13:37:05 +0300

    refactor: added option to download data by page

--- src/app/core/service/global-giving-api.service.ts
+++ src/app/core/service/global-giving-api.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { IUserToken } from '../models/users.models';
-import { BASE_URL, USER_API, API_KEY } from '../../shared/constants/constsnts';
+import { IProjects } from '../models/projects.model';
+import { BASE_URL } from '../../shared/constants/constsnts';
 
 const GLOBAL_GIVIN = {
   TOKEN: `${BASE_URL}/userservice/tokens`,
@@ -18,15 +20,34 @@ export class GlobalGivingApiService {
     headers: new HttpHeaders({ Accept: 'application/json', 'Content-Type': 'application/json' })
   };
 
-  body = JSON.stringify({ auth_request: USER_API, api_key: API_KEY });
+  body = { auth_request: 'USER_API' };
 
   constructor(private http: HttpClient) {}
+
+  handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    // later replace with component
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
   public getAccessToken(): Observable<IUserToken> {
     return this.http.post<IUserToken>(GLOBAL_GIVIN.TOKEN, this.body);
   }
 
-  public getActiveProjectsForCountry(iso3166CountryCode: string): Observable<any> {
-    return this.http.get<any>(`${GLOBAL_GIVIN.ACTIVE_FOR_COUNTRY(iso3166CountryCode)}`);
+  public getActiveProjectsForCountry(iso3166CountryCode: string, nextProjectID?: number): Observable<IProjects> {
+    const id: number = nextProjectID || 1;
+    const options = { params: new HttpParams({ fromString: `&nextProjectId=${id}` }) };
+    return this.http.get<IProjects>(`${GLOBAL_GIVIN.ACTIVE_FOR_COUNTRY(iso3166CountryCode)}`, options).pipe(
+      retry(3),
+      catchError((e) => this.handleError(e))
+    );
   }
 }
