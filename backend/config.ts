cc43fbd1716833f964b312d767ebec2951d53422
Anna Tolstova <anna_tolstova@hotmail.com>
2021-01-21 12:18:02 +0200

    fix: use dotenv on dev only

--- backend/config.ts
+++ backend/config.ts
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
 
-const envConfig = dotenv.config();
-if (envConfig.error) {
-  console.error(envConfig.error);
+console.log('config NODE_ENV:', process.env.NODE_ENV);
+const environment = process.env.NODE_ENV || 'development';
+if (environment === 'development') {
+  const envConfig = dotenv.config();
+  if (envConfig.error) {
+    console.error(envConfig.error);
+  }
 }
 
 export const { MONGO_CONNECTION_STRING, SECRET_TOKEN } = process.env;
