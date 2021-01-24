76f6287d4725eb723df5a7fd1048ed24b0dd2453
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-01-24 12:10:49 +0300

    feat: issues#16 add a list of projects on the selected theme

--- src/app/base/components/project-list/project-list.component.ts
+++ src/app/base/components/project-list/project-list.component.ts
@@ -1,10 +1,13 @@
 import {
- Component, OnInit, Input, OnDestroy
+ Component, OnInit, OnDestroy
 } from '@angular/core';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { catchError, takeUntil } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+import { EMPTY, Subject, Subscription } from 'rxjs';
 
-import { IProject, IProjects } from 'src/app/core/models/projects.model';
+import {
+ IProject, IQueryOptions, ISearchResults
+} from 'src/app/core/models/projects.model';
 import { GlobalGivingApiService } from '../../../core/service/global-giving-api.service';
 
 @Component({
@@ -15,50 +18,81 @@ import { GlobalGivingApiService } from '../../../core/service/global-giving-api.
 export class ProjectListComponent implements OnInit, OnDestroy {
   destroy$: Subject<boolean> = new Subject<boolean>();
 
-  country = 'UA';
+  private subscriptions: Subscription[] = [];
 
-  @Input() dataProjects!: IProject[];
+  set subscription(sb: Subscription) { this.subscriptions.push(sb) };
+
+  queryOptions: IQueryOptions = {
+    keyWords: '*',
+    startNumber: 0,
+    iso3166CountryCode: '',
+    theme: ''
+  };
+
+  countProjects = 0;
+
+  // searchQuery: FormControl = new FormControl();
+
+  error = false;
 
   hasNext = false;
 
-  nextProjectId = 1;
+  errorMessage = '';
 
-  nextProjectMessage = '';
+  dataProjects!: IProject[];
 
-  constructor(private globalGivingApiService: GlobalGivingApiService) {}
+  // country = 'UA';
+
+  constructor(
+    private globalGivingApiService: GlobalGivingApiService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.globalGivingApiService
-      .getActiveProjectsForCountry(this.country)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((data: IProjects) => {
-        if (data.projects.hasNext !== undefined) {
-          this.nextProjectId = data.projects.nextProjectId;
-        }
-        this.hasNext = data.projects.hasNext || false;
-        this.dataProjects = data.projects.project;
-        console.log(this.dataProjects);
-      });
+    this.subscription = this.route.params.subscribe((params): void => {
+      this.queryOptions.theme = params.id as string;
+      console.log(this.queryOptions);
+    });
+
+    this.globalGivingApiService.getActiveProjectsByKeyWords(this.queryOptions).pipe(
+      takeUntil(this.destroy$),
+      catchError((error) => {
+        this.dataProjects = [];
+        this.error = true;
+        return EMPTY;
+      })
+    ).subscribe((results: ISearchResults) => {
+      if (results.search.response.numberFound > 0) {
+        this.countProjects = results.search.response.numberFound;
+        this.dataProjects = results.search.response.projects.project;
+        this.error = false;
+        this.errorMessage = '';
+        console.log(this.countProjects, this.dataProjects);
+      } else {
+        this.errorMessage = 'No projects found! Please try again.';
+      }
+    });
   }
 
   public nextPage(): void {
-    if (this.hasNext) {
-      this.globalGivingApiService
-        .getActiveProjectsForCountry(this.country, this.nextProjectId)
-        .pipe(takeUntil(this.destroy$))
-        .subscribe((data: IProjects) => {
-          if (data.projects.hasNext !== undefined) {
-            this.nextProjectId = data.projects.nextProjectId;
-          }
-          this.hasNext = data.projects.hasNext || false;
-          this.nextProjectMessage = !this.hasNext ? 'There are no more active projects!!' : '';
-          this.dataProjects = this.dataProjects.concat(data.projects.project);
-        });
-    }
+    // if (this.hasNext) {
+    //   this.globalGivingApiService
+    //     .getActiveProjectsForCountry(this.country, this.nextProjectId)
+    //     .pipe(takeUntil(this.destroy$))
+    //     .subscribe((data: IProjects) => {
+    //       if (data.projects.hasNext !== undefined) {
+    //         this.nextProjectId = data.projects.nextProjectId;
+    //       }
+    //       this.hasNext = data.projects.hasNext || false;
+    //       this.nextProjectMessage = !this.hasNext ? 'There are no more active projects!!' : '';
+    //       this.dataProjects = this.dataProjects.concat(data.projects.project);
+    //     });
+    // }
   }
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
+    this.subscriptions.forEach((sb) => sb.unsubscribe())
   }
 }
