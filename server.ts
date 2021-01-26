55f144a15c666b0ecf9c2c1d2365b1cf5f0360ad
Anna Tolstova <anna_tolstova@hotmail.com>
2021-01-26 19:02:44 +0200

    fix: change console to logger

--- server.ts
+++ server.ts
@@ -1,8 +1,6 @@
 import * as http from 'http';
 import app from './backend/app.js';
-
-// import debugmod from 'debug';
-// const debug = debugmod('rsclone-node');
+import logger from './backend/logger.js';
 
 // Normalize a port.
 function normalizePort(val: string): string | number | boolean {
@@ -39,11 +37,11 @@ function onError(error: NodeJS.ErrnoException) {
   // TODO error logging!
   switch (error.code) {
     case 'EACCES':
-      console.error(`${bind} requires elevated privileges`);
+      logger.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(`${bind} is already in use`);
+      logger.error(`${bind} is already in use`);
       process.exit(1);
       break;
     default:
@@ -58,7 +56,7 @@ function onListening() {
   if (addr) {
     bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
   }
-  console.log(`Listening on ${bind}`);
+  logger.info(`Server is listening on ${bind}`);
 }
 
 server.listen(port);
