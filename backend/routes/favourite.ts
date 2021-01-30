8933c9060b8cfb1b3a1d353c54c5f0a4221cda0a
Anna Tolstova <anna_tolstova@hotmail.com>
2021-01-30 13:45:18 +0200

    feat: add token verification middleware

--- backend/routes/favourite.ts
+++ backend/routes/favourite.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import { verifyToken } from '../middleware/webtoken-verifier.js'
 
 const router = express.Router();
 
-router.post('/add', (req: express.Request) => {
+router.post('/add', verifyToken, (req: express.Request) => {
   console.log('fav route', req.body);
 });
 
