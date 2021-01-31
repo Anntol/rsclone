3a849b304a485673e7a778bfee14a3fe6689346f
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-01-31 16:15:06 +0300

    fix: filter button visibility

--- src/app/base/pages/projects-page/projects-page.component.ts
+++ src/app/base/pages/projects-page/projects-page.component.ts
@@ -1,36 +1,69 @@
 import {
  AfterViewChecked, Component, ViewChild, ChangeDetectorRef, OnInit
 } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 import { Sort } from '@angular/material/sort';
 import { FormControl } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
-// import { IProject, IQueryOptions } from '../../../core/models/projects.model';
 import { SelectLangComponent } from '../../../shared/components/select-lang/select-lang.component';
-import { DataService } from 'src/app/core/service/data.service';
+import { DataService } from '../../../core/service/data.service';
 
 @Component({
   selector: 'app-projects-page',
   templateUrl: './projects-page.component.html',
   styleUrls: ['./projects-page.component.scss', '../../../../theme/buttons.scss', '../../../../theme/stacks.scss']
 })
-export class ProjectsPageComponent implements AfterViewChecked {
+export class ProjectsPageComponent implements AfterViewChecked, OnInit {
   @ViewChild(SelectLangComponent) selectLang!: SelectLangComponent;
 
   public searchQuery: FormControl = new FormControl();
 
   public sort!: Sort;
 
+  public isVisibleFilterButton = false;
+
+  public isSort = false;
+
+  public isSearch = false;
+
   constructor(
     public translate: TranslateService,
     public router: Router,
+    private route: ActivatedRoute,
     public dataService: DataService,
     private cdr: ChangeDetectorRef
   ) {}
 
   public sortData(sort: Sort): void {
     this.dataService.setSortOptions(sort);
+  }
+
+  public changeFilter(el: HTMLElement): void {
+    if(el.getAttribute('data-filter') === 'search') {
+      this.isSearch = !this.isSearch;
+      if (this.isSearch === this.isSort) {
+        this.isSort = !this.isSort;
+      }
+    } else {
+      this.isSort = !this.isSort;
+      if (this.isSearch === this.isSort) {
+        this.isSearch = !this.isSearch;
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        console.log(e.url);
+        if (e.url.includes('/projects')) {
+          this.isVisibleFilterButton = true;
+        } else {
+          this.isVisibleFilterButton = false;
+        }
+      }
+    });
   }
 
   ngAfterViewChecked(): void {
