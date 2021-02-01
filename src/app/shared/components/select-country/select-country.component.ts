9a2efa3870be5d3d3a40dd501d356f6ae86d459c
coriander31415 <coriander31415@gmail.com>
2021-02-01 10:22:11 +0200

    feat: add countries select

--- src/app/shared/components/select-country/select-country.component.ts
+++ src/app/shared/components/select-country/select-country.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as i18nIsoCountries from 'i18n-iso-countries';
+
+declare const require: (arg0: string) => { locale: string; countries: { [alpha2Key: string]: string | string[]; }; };
 
 @Component({
   selector: 'app-select-country',
   templateUrl: './select-country.component.html',
   styleUrls: ['./select-country.component.scss']
 })
-export class SelectCountryComponent {}
+export class SelectCountryComponent implements OnInit {
+  myLanguage = 'uk';
+
+  countries = {};
+
+  ngOnInit(): void {
+    i18nIsoCountries.registerLocale(require("i18n-iso-countries/langs/en.json"));
+    i18nIsoCountries.registerLocale(require("i18n-iso-countries/langs/uk.json"));
+    i18nIsoCountries.registerLocale(require("i18n-iso-countries/langs/be.json"));
+
+    switch (this.myLanguage) {
+      case 'en':
+        this.countries = i18nIsoCountries.getNames('en', { select: 'official' });
+        break;
+      case 'be':
+        this.countries = i18nIsoCountries.getNames('be', { select: 'official' });
+        break;
+      case 'uk':
+        this.countries = i18nIsoCountries.getNames('uk', { select: 'official' });
+        break;
+      default:
+        console.log('Language is not defined');
+    }
+  }
+}
