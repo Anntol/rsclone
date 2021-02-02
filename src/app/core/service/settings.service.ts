969eb7d42d9bec5d19dbcca723e5dd34ec111136
Anna Tolstova <anna_tolstova@hotmail.com>
2021-02-02 18:51:33 +0200

    feat: error handling in settings service

--- src/app/core/service/settings.service.ts
+++ src/app/core/service/settings.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorComponent } from '../../shared/components/error/error.component';
 import { environment } from '../../../environments/environment';
 import { IFavourite } from '../models/favourite.model';
 import { IUserInfo } from '../models/userinfo.model';
@@ -9,36 +12,41 @@ import { IUserInfo } from '../models/userinfo.model';
   providedIn: 'root'
 })
 export class SettingsService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private dialog: MatDialog) {}
 
   serverUrl = environment.serverUrl;
 
+  handleError(error: HttpErrorResponse): Observable<never> {
+    console.log('handler: ', error);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const msg = error.error.message as string;
+    const errorMessage = msg || "An unknown error occurred!";
+    this.dialog.open(ErrorComponent, { data: { message: errorMessage } });
+    return throwError(errorMessage);
+  }
+
   addUserFavourite(favProject: IFavourite): void {
     this.http.post(`${this.serverUrl}/api/userFavourite/`, favProject)
-    .subscribe(() => {
-      console.log('UserFavourite added');
-    }, (e) => console.error(e)); // TODO error handling
+      .pipe(catchError((e) => this.handleError(e)));
   }
 
   removeUserFavourite(projectId: string): void {
     this.http.delete(`${this.serverUrl}/api/userFavourite/${projectId}`)
-    .subscribe(() => {
-      console.log('UserFavourite removed');
-    }, (e) => console.error(e));
+      .pipe(catchError((e) => this.handleError(e)));
   }
 
   getUserFavourites(): Observable<{message: string, favourites: IFavourite[]}> {
-    return this.http.get< { message: string, favourites: IFavourite[]}>(`${this.serverUrl}/api/userFavourite/`);
+    return this.http.get< { message: string, favourites: IFavourite[]}>(`${this.serverUrl}/api/userFavourite/`)
+      .pipe(catchError((e) => this.handleError(e)));
   }
 
   SaveUserInfo(userInfo: IUserInfo): void {
     this.http.post(`${this.serverUrl}/api/userSettings/`, userInfo)
-    .subscribe(() => {
-      console.log('UserInfo saved');
-    }, (e) => console.error(e)); // TODO error handling
+      .pipe(catchError((e) => this.handleError(e)));
   }
 
   getUserInfoSettings(): Observable<{message: string, userInfo: IUserInfo}> {
-    return this.http.get< { message: string, userInfo: IUserInfo}>(`${this.serverUrl}/api/userSettings/`);
+    return this.http.get< { message: string, userInfo: IUserInfo}>(`${this.serverUrl}/api/userSettings/`)
+    .pipe(catchError((e) => this.handleError(e)));
   }
 }
