2434c6ee30ed3ad661cd5adb5599b18e6f2f7422
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-02-04 11:08:40 +0300

    fix: disabled 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainPageComponent } from './base/pages/main-page/main-page.component';
-import { NotFoundPageComponent } from './base/components/not-found-page/not-found-page.component';
+// import { NotFoundPageComponent } from './base/components/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   { path: 'home', component: MainPageComponent },
   {
-    path: 'page',
+    path: '',
     loadChildren: () => import('src/app/base/base.module').then((m) => m.BaseModule)
   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', component: NotFoundPageComponent } // TODO Wildcard route for a 404 page
+  // { path: '**', component: NotFoundPageComponent } // TODO Wildcard route for a 404 page
 ];
 
 @NgModule({
