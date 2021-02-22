c19923dcc2dba27e2e62776e9ac08db5e123de9c
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-02-22 22:05:47 +0300

    refactor: globe add to the core module

---
+++ src/app/core/components/globe/globe.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-globe',
+  templateUrl: './globe.component.html',
+  styleUrls: [
+    './globe.component.scss',
+    '../../../../theme/globo.scss',
+    '../../../../theme/noselect.scss'
+  ]
+})
+export class GlobeComponent {
+
+}
