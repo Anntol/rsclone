022a491c15d8a6a26a4dd958157804ac605ef88a
coriander31415 <coriander31415@gmail.com>
2021-02-26 11:07:54 +0200

    test: render markers based on GetAllOrg reqst

--- src/app/map/components/map/map.component.ts
+++ src/app/map/components/map/map.component.ts
@@ -15,14 +15,15 @@ export class MapComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.initMap();
     // this.markerService.makeCapitalMarkers(this.map);
-    this.markerService.makeCapitalCircleMarkers(this.map);
+    // this.markerService.makeCapitalCircleMarkers(this.map);
+    this.markerService.makeProjectsMarkers(this.map);
   }
 
   private initMap(): void {
     this.map = L.map('map', {
       // center: [50.45466, 30.5238], // Kyiv
       center: [39.8282, -98.5795], // USA
-      zoom: 4
+      zoom: 2
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
--- src/app/map/services/marker.service.ts
+++ src/app/map/services/marker.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
@@ -6,7 +7,9 @@ import * as L from 'leaflet';
   providedIn: 'root'
 })
 export class MarkerService {
-  capitals: string = '../../assets/test/us_capitals.geojson';
+  // capitals: string = '../../assets/test/us_capitals.geojson';
+  projectsLink =
+    'https://api.globalgiving.org/api/public/projectservice/all/projects/active?api_key=9f4d5d1b-2ecc-4d62-972b-d5bc07df5d92';
 
   static ScaledRadius(val: number, maxVal: number): number {
     return 20 * (val / maxVal);
@@ -14,30 +17,45 @@ export class MarkerService {
 
   constructor(private http: HttpClient) {}
 
-  // test geo-markers
-  makeCapitalMarkers(map: L.Map): void {
-    this.http.get(this.capitals).subscribe((res: any) => {
-      for (const c of res.features) {
-        const lat = c.geometry.coordinates[0];
-        const lon = c.geometry.coordinates[1];
-        const marker = L.marker([lon, lat]).addTo(map);
-      }
+  makeProjectsMarkers(map: L.Map): void {
+    this.http.get(this.projectsLink).subscribe((res: any) => {
+      res.projects.project.forEach((c: any) => {
+        const lat = c.latitude;
+        const lon = c.longitude;
+        const popupInfo = `${c.title} (${c.contactCity}, ${c.contactCountry})`;
+        console.log('country, latitude, longitude: ', c.country, c.latitude, c.longitude);
+        if (lon !== undefined && lat !== undefined) {
+          const marker = L.marker([lat, lon]).addTo(map);
+          marker.bindPopup(popupInfo).openPopup();
+        }
+      });
     });
   }
+
+  // test geo-markers
+  // makeCapitalMarkers(map: L.Map): void {
+  //   this.http.get(this.capitals).subscribe((res: any) => {
+  //     for (const c of res.features) {
+  //       const lat = c.geometry.coordinates[0];
+  //       const lon = c.geometry.coordinates[1];
+  //       const marker = L.marker([lon, lat]).addTo(map);
+  //     }
+  //   });
+  // }
 
   // test circle markers resized by population
-  makeCapitalCircleMarkers(map: L.Map): void {
-    this.http.get(this.capitals).subscribe((res: any) => {
-      // Find the maximum population to scale the radii by.
-      const maxVal = Math.max(...res.features.map((x: any) => x.properties.population), 0);
+  // makeCapitalCircleMarkers(map: L.Map): void {
+  //   this.http.get(this.capitals).subscribe((res: any) => {
+  //     // Find the maximum population to scale the radii by.
+  //     const maxVal = Math.max(...res.features.map((x: any) => x.properties.population), 0);
 
-      for (const c of res.features) {
-        const lat = c.geometry.coordinates[0];
-        const lon = c.geometry.coordinates[1];
-        const circle = L.circleMarker([lon, lat], {
-          radius: MarkerService.ScaledRadius(c.properties.population, maxVal)
-        }).addTo(map);
-      }
-    });
-  }
+  //     for (const c of res.features) {
+  //       const lat = c.geometry.coordinates[0];
+  //       const lon = c.geometry.coordinates[1];
+  //       const circle = L.circleMarker([lon, lat], {
+  //         radius: MarkerService.ScaledRadius(c.properties.population, maxVal)
+  //       }).addTo(map);
+  //     }
+  //   });
+  // }
 }
