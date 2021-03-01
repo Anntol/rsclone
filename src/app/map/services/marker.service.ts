659b1d40fef7971224f822268ff241935ac26eba
coriander31415 <coriander31415@gmail.com>
2021-03-01 15:57:21 +0200

    test: geoJSON markers from LocalStorage

--- src/app/map/services/marker.service.ts
+++ src/app/map/services/marker.service.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable max-len */
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
 
@@ -8,8 +12,10 @@ import * as L from 'leaflet';
 })
 export class MarkerService {
   // capitals: string = '../../assets/test/us_capitals.geojson';
-  projectsLink =
-    'https://api.globalgiving.org/api/public/projectservice/all/projects/active?api_key=9f4d5d1b-2ecc-4d62-972b-d5bc07df5d92';
+  // projectsLink =
+  //   'https://api.globalgiving.org/api/public/projectservice/all/projects/active?api_key=9f4d5d1b-2ecc-4d62-972b-d5bc07df5d92';
+
+  geoJsonFromLocal = localStorage.getItem('rs_geoJson');
 
   static ScaledRadius(val: number, maxVal: number): number {
     return 20 * (val / maxVal);
@@ -17,20 +23,42 @@ export class MarkerService {
 
   constructor(private http: HttpClient) {}
 
+  // test markers from geoJSON
   makeProjectsMarkers(map: L.Map): void {
-    this.http.get(this.projectsLink).subscribe((res: any) => {
-      res.projects.project.forEach((c: any) => {
-        const lat = c.latitude;
-        const lon = c.longitude;
-        const popupInfo = `${c.title} (${c.contactCity}, ${c.contactCountry})`;
-        console.log('country, latitude, longitude: ', c.country, c.latitude, c.longitude);
-        if (lon !== undefined && lat !== undefined) {
-          const marker = L.marker([lat, lon]).addTo(map);
-          marker.bindPopup(popupInfo).openPopup();
+    if (this.geoJsonFromLocal !== null) {
+      // console.log(this.geoJsonFromLocal);
+    const projGeoJson = JSON.parse(this.geoJsonFromLocal);
+      console.log("projGeoJson: ", projGeoJson);
+      // this.http.get(projGeoJson).subscribe((res: any) => {
+        for (const c of projGeoJson.features) {
+          const lat = c.geometry.coordinates[0];
+          const lon = c.geometry.coordinates[1];
+          const popupInfo = `${c.properties.title} (${c.properties.contactCity}, ${c.properties.contactCountry})`;
+          console.log("Project ID:", c.properties.projectId, "Cordinates:", lat, lon);
+          if (lon !== null || lat !== null) {
+            const marker = L.marker([lon, lat]).addTo(map);
+            marker.bindPopup(popupInfo).openPopup();
+          }
         }
-      });
-    });
-  }
+      // });
+    }
+}
+
+  // test markers from http link
+  // makeProjectsMarkers(map: L.Map): void {
+  //   this.http.get(this.projectsLink).subscribe((res: any) => {
+  //     res.projects.project.forEach((c: any) => {
+  //       const lat = c.latitude;
+  //       const lon = c.longitude;
+  //       const popupInfo = `${c.title} (${c.contactCity}, ${c.contactCountry})`;
+  //       console.log('country, latitude, longitude: ', c.country, c.latitude, c.longitude);
+  //       if (lon !== undefined && lat !== undefined) {
+  //         const marker = L.marker([lat, lon]).addTo(map);
+  //         marker.bindPopup(popupInfo).openPopup();
+  //       }
+  //     });
+  //   });
+  // }
 
   // test geo-markers
   // makeCapitalMarkers(map: L.Map): void {
